1f7d21455b4e8137d63296f129ef0d478b0cdaea
Nicholas Osorio <nicholas.osorio98@gmail.com>
2025-08-06 14:43:36 -0400

    Deploy built site

---
+++ data/images.js
@@ -0,0 +1,95 @@
+const images = [
+  {
+    file: "img1.jpg",
+    context: "Edwin Osorio with congressman Peter King",
+  },
+  {
+    file: "img2.jpg",
+    context: "Edwin Osorio with Sean Patrick Maloney",
+  },
+  {
+    file: "img3.jpg",
+    context: "Edwin Osorio with congressman Adriano Espaillat",
+  },
+  {
+    file: "img4.jpg",
+    context:
+      "Edwin Osorio with congressman Dan Goldman and senator Kirsten Gillibrand.",
+  },
+  {
+    file: "img5.jpg",
+    context: "Edwin Osorio with congressman Lee Zeldin",
+  },
+  {
+    file: "img6.jpg",
+    context: "Edwin Osorio with Max Rose",
+  },
+  {
+    file: "img7.jpg",
+    context: "Edwin Osorio with congressman Hakeem Jeffries",
+  },
+  {
+    file: "img8.jpg",
+    context:
+      "Edwin Osorio at a press conference with congressman Dan Goldman and SSA executive Alex Lawson",
+  },
+  {
+    file: "img9.jpg",
+    context: "Edwin Osorio with congressman George Latimer",
+  },
+  {
+    file: "img10.png",
+    context:
+      "Edwin Osorio at a Social Security briefing with congressman Thomas Suozzi, congresswoman Laura Gillan, and former SSA commissioner Martin O'Malley at LIU Post",
+  },
+  {
+    file: "img11.png",
+    context: "Edwin Osorio speaking at an AFGE meeting",
+  },
+  {
+    file: "img12.jpg",
+    context: "Edwin Osorio lobbying in D.C.",
+  },
+  {
+    file: "img13.jpg",
+    context: "Edwin Osorio with Sean Patrick Maloney",
+  },
+  {
+    file: "img14.JPG",
+    context: "Edwin Osorio speaking with congressman Hakeem Jeffreys",
+  },
+  {
+    file: "img15.jpg",
+    context: "Edwin Osorio and his wife Cristina Osorio on the Love Boat",
+  },
+  {
+    file: "img16.jpg",
+    context: "Edwin Osorio with his family meeting Paul Stanley",
+  },
+  {
+    file: "img17.png",
+    context: "Edwin Osorio with Max Rose",
+  },
+  {
+    file: "img18.png",
+    context: "Edwin Osorio with congressman Gregory Meeks",
+  },
+  {
+    file: "img19.png",
+    context: "Edwin Osorio with congresswoman Grace Meng",
+  },
+  {
+    file: "img20.png",
+    context: "Edwin Osorio with congressman Hakeem Jeffries",
+  },
+  {
+    file: "img21.png",
+    context: "Edwin Osorio with congresswoman Carolyne Maloney",
+  },
+  {
+    file: "img22.png",
+    context: "Edwin Osorio with congressman Adriano Espaillat",
+  },
+];
+
+export default images;
